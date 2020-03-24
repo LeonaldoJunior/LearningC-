75c94037546af97383f9d896634a86d2aa2b1b83
Leonaldo <leonaldo.barbosa@gmail.com>
2020-03-24 12:57:30 -0300

    Aula 02 Tipos de Variáveis

---
+++ Aula02TiposDeVariáveis/Aula02TiposDeVariáveis/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula02TiposDeVariáveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variável tipo int
+            var numeroInteiro2 = 1;
+            //x = "333";
+            int numeroInteiro1 = 1;
+            //i = "s";
+            Console.WriteLine("Variável tipo int: ");
+            Console.WriteLine(numeroInteiro1);
+            Console.WriteLine(numeroInteiro2);
+
+            //Variável tipo string
+            string nome1 = "Leonaldo 01";
+            var    nome2 = "Leonaldo 02";
+            Console.WriteLine("");
+            Console.WriteLine("Variável tipo string: ");
+            Console.WriteLine(nome1);
+            Console.WriteLine(nome2);
+
+            //Variável tipo float
+            var   numeroVirgula1 = 1.5f;
+            float numeroVirgula2 = 1.5f;
+            Console.WriteLine("");
+            Console.WriteLine("Variável tipo float: ");
+            Console.WriteLine(numeroVirgula1);
+            Console.WriteLine(numeroVirgula2);
+
+            //Variável tipo double
+            var     numeroMaisCasasDecimais1 = 1.5;
+            double  numeroMaisCasasDecimais2 = 1.5;
+            Console.WriteLine("");
+            Console.WriteLine("Variável tipo double: ");
+            Console.WriteLine(numeroMaisCasasDecimais1);
+            Console.WriteLine(numeroMaisCasasDecimais2);
+
+            //Variável tipo char
+            char letra1 = 'b';
+            var  letra2 = 'b';
+            Console.WriteLine("");
+            Console.WriteLine("Variável tipo char: ");
+            Console.WriteLine(letra1);
+            Console.WriteLine(letra2);
+
+            //Variável tipo string
+            var    palavra1 = "b";
+            string palavra2 = "b";
+            Console.WriteLine("");
+            Console.WriteLine("Variável tipo string: ");
+            Console.WriteLine(palavra1);
+            Console.WriteLine(palavra2);
+
+
+
+
+        }
+    }
+}
