e3139e925933b96bd8fb9a74504e13052393a335
Leonaldo <leonaldo.barbosa@gmail.com>
2020-03-25 11:52:09 -0300

    Aula 07 Funções recursivas

---
+++ Aula07FuncoesRecursivas/Aula07FuncoesRecursivas/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula07FuncoesRecursivas
+{
+    class Program
+    {
+        private static void LerArquivo(int numeroArquivo)
+        {
+            string caminhoArquivo = @"C:\Users\Leonaldo\Desktop\LearningC-\Arquivos\arq" + numeroArquivo + ".txt";
+            if (File.Exists(caminhoArquivo))
+            {
+                using (StreamReader arquivo = File.OpenText(caminhoArquivo))
+                {
+                    string linha;
+                    while ((linha = arquivo.ReadLine()) != null)
+                    {
+                        Console.WriteLine(linha);
+                    }
+                }
+            }
+            string caminhoArquivo2 = @"C:\Users\Leonaldo\Desktop\LearningC-\Arquivos\arq" + (numeroArquivo + 1)+ ".txt";
+            if (File.Exists(caminhoArquivo2))
+            {
+                LerArquivo(numeroArquivo + 1);
+            }
+
+                
+        } 
+
+        static void Main(string[] args)
+        {
+
+            LerArquivo(1);
+            Console.Read();
+        }
+    }
+}
